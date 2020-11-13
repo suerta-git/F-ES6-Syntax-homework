c29a11481e0bead502f18fbf4224fc3380f2aa83
Jindong Zhang <jindong.zhang@JindongdeMacBook-Pro.local>
2020-11-13 10:44:25 +0800

    [Zhang Jindong] feat: complete practices

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = data => {
+    const columns = data.column.map(column => column.name);
+    return data.data.map(record => {
+        const result = {};
+        record.forEach((element, index) => {
+            result[columns[index]] = element;
+        });
+        return result;
+    })
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,15 @@
-const inject
+const inject = (items, sections) => {
+    const sortedSections = [...sections].sort((left, right) => left.index - right.index);
+    const result = [...items];
+    let sectionIndex = sortedSections.length - 1;
+    for (let i = result.length - 1; i >= 0; i--) {
+        if (sortedSections[sectionIndex].index === i) {
+            result.splice(i, 0, sortedSections[sectionIndex].content);
+            if (--sectionIndex < 0) {
+                break;
+            }
+        }
+    }
+    return result;
+}
 export { inject };
